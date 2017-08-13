371af80ede861a26fe9a7540a72bb40306d50035
Rob Lendvai <lendvai@gmail.com>
2017-08-13 16:14:22 -0400

    stuck on creating a scheduler

--- Scheduler3.py
+++ Scheduler3.py
@@ -1,5 +1,3 @@
-from turtle import *
-
 import names
 import random
 from pendulum import *
@@ -19,11 +17,12 @@ class Patient:
         self.name = names.get_full_name()
 
 
-class Slot:
+class AppSlot:
     def __init__(self, begin_time, length):
         self.appointment = None
         self.type = "General"
         self.begin_time = begin_time
+        self.length = length
         if length > 0:
             self.fill(Appointment(Patient(), length, begin_time))
 
@@ -61,7 +60,7 @@ class Slot:
 
 
 class Schedule:
-    def __init__(self, start_time, num_days=3, num_slots=3, duration=30, density_percent=80):
+    def __init__(self, start_time, num_days=1, num_slots=1, duration=30, density_percent=80):
 
         self.start_time = start_time
         self.cal_times = []
@@ -72,35 +71,35 @@ class Schedule:
                     slot_time = self.start_time.add(days=i)
                     slot_time = slot_time.add(minutes=(n * duration))
                     if random.random() <= density_percent / 100:
-                        self.cal_times.append((slot_time, Slot(slot_time, duration)))
+                        self.cal_times.append((slot_time, AppSlot(slot_time, duration)))
                     else:
-                        self.cal_times.append((slot_time, Slot(slot_time, 0)))
+                        self.cal_times.append((slot_time, AppSlot(slot_time, 0)))
 
-
-
-    def makeQ(self):
-        Q=[x for x in self.cal_times if x[1]!=None]
-        return Q
-
-    def showQ(self):
-        self.show()
-
-
-
-    def show(self):
+    def cal_times_printer(self, cal_times):
 
         last_seen_day = None
-        print("\n**** SCHEDULE STARTING: ", self.start_time.format('MM/DD'), " ***\n")
-        for cal_time in self.cal_times:
+        start_time = cal_times[0][0]
+        print("\n**** SCHEDULE STARTING: ", start_time.format('MM/DD'), " ***\n")
+        for cal_time in cal_times:
             if last_seen_day != cal_time[0].format('MM/DD'):
                 print("--------", cal_time[0].format('MM/DD'), "-------")
             print(cal_time[1].status_string())
             last_seen_day = cal_time[0].format('MM/DD')
 
-    def cancel_appointment(self, time):
+    def show(self):
+        self.cal_times_printer(self.cal_times)
 
-        id = self.find_slot_id(time)
-        self.cal_times[id][1].unfill()
+    def show_waitlist(self):
+        self.cal_times_printer(self.make_waitlist())
+        pass
+
+    def make_waitlist(self):
+        return [x for x in self.cal_times if x[1].filled_status()]
+
+    def cancel_appointment(self, time):
+    # unfill the slot at this time in the schedule
+
+        dict(self.cal_times)[time].unfill()
 
     def find_slot_id(self, time):
         for i in range(len(self.cal_times)):
@@ -108,7 +107,31 @@ class Schedule:
                 return i
         return False
 
+    #get the time of a time in an appointment
+    def get_time(self, slot):
+        pass
 
+    def refill(self, time):
+        w = self.make_waitlist()
+        new_slot = w.getNext()
+        id = self.find_slot_id(time)
+        self.cal_times[id] = new_slot
+
+
+class Scheduler:
+    def __init__(self, cal_times):
+        self.cal_times = cal_times
+
+    def makeQ(self,cal_times):
+        q=[x for x in cal_times if x[1].filled_status()]
+        return q
+
+    def getQ(self):
+        return makeQ(self.cal_times)
+
+    def getNext(self):
+        self.schedule.cancel_appointment(self.schedule.cal_times[-1][0])
+        return self.schedule.cal_times.pop()
 
 
 
@@ -135,12 +158,16 @@ def main():
     pendulum.set_formatter('alternative')
 
     start_time = Pendulum(2017, 8, 1, 9, tzinfo='America/New_York')
-    S = Schedule(start_time)
+    S = Schedule(start_time, num_days=2, num_slots=5, duration=30, density_percent=50)
     S.show()
+
     seek_time = Pendulum(2017, 8, 1, 9, 30, tzinfo='America/New_York')
+
     S.cancel_appointment(seek_time)
+
     S.show()
-    S.showQ()
+
+    S.show_waitlist()
 
 
 main()
